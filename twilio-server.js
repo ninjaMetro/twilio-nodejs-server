f4eddf47c17e04b29a1872771e88023925381eb6
Shemm Junior <juniorshemm@gmail.com>
2020-12-30 19:56:38 +0300

    Update twilio-server.js

--- twilio-server.js
+++ twilio-server.js
@@ -47,7 +47,32 @@ app.get('/getsms', (req, res) => {
 //twilioClient.messages.each(messages => messages.remove().then().done());
 
 
-/** TODO: Send SMS with CRON JOB everyday at 1200 HRS */
+/** Send SMS with CRON JOB everyday at 1200 HRS */
+var everyday = schedule.scheduleJob({ hour: 12, minute: 00 }, function() {
+
+    /**
+     * Here you can add custom query of your own for phone numbers from database and pass them to twilio client
+     * 
+     */
+    const phoneNumber = req.body.phoneNumber;
+    const text = req.body.text;
+
+    twilioClient.messages.create({
+        to: phoneNumber,
+        from: process.env.NUMBER,
+        body: text
+    }, function(error, response) {
+        if(error) {
+            console.log(error.message);
+            res.json({ error: error.message})
+        } else {
+            console.log(response);
+            res.json({ success: response})
+        }
+    })
+});
+
+
 
 app.listen(3000, () => console.log('Twilio Server Started Successfully'));
 
